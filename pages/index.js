e3477444fa76464dd932a7686d114440c142a7cc
colorlessenergy <brianmunozdev@gmail.com>
2021-10-11 07:49:22 -0700

    create HTML structure for a task

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,28 @@ export default function Home () {
                         placeholder="filter..." />
                 </div>
 
+                <div className="card flex flex-direction-column p-1 b-1-black">
+                    <div className="flex justify-content-between align-items-center">
+                        <div className="text-3">
+                            get groceries
+                        </div>
+
+                        <div className="square background-color-black"></div>
+                    </div>
+
+                    <p className="card__text m-0 mb-auto text-2">
+                        1 bread 2 bananas 5 can of beans 1 bag of oranges
+                    </p>
+
+                    <div className="flex justify-content-between align-items-center">
+                        <div className="flex justify-content-center align-items-center tag background-color-red">
+                            urgent
+                        </div>
+
+                        <div className="square b-1-black"></div>
+                    </div>
+                </div>
+
                 <Modal isOpen={ isAddTaskModalOpen } toggleModal={ toggleAddTaskModal }>
                     <AddTask toggleModal={ toggleAddTaskModal } />
                 </Modal>
