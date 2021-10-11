ad625215d0854aee7e2636ee51e43cda278eec93
colorlessenergy <brianmunozdev@gmail.com>
2021-10-10 22:06:11 -0700

    add functions to get and create tasks

---
+++ shared/localStorage/task.js
@@ -0,0 +1,26 @@
+export const getTasks = () => {
+    if (!localStorage.getItem('tasks')) {
+        localStorage.setItem('tasks', JSON.stringify([]));
+    }
+
+    if (!localStorage.getItem('ID')) {
+        localStorage.setItem('ID', JSON.stringify(0));
+    }
+
+    return localStorage.getItem('tasks');
+}
+
+export const createTask = (task) => {
+    let ID = JSON.parse(localStorage.getItem('ID'));
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    ID += 1;
+    task = {
+        ID,
+        ...task
+    };
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('ID', JSON.stringify(ID));
+}
